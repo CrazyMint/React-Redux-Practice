b8a04cd8ec78b7eee61a464187f8707586c3a746
CrazyMint <w1938721841@gmail.com>
2023-06-06 15:24:06 -0400

    refactor

--- src/components/hoc/myConnect.js
+++ src/components/hoc/myConnect.js
@@ -5,22 +5,28 @@ import React, { Component } from "react";
 export const myConnect =
 	(mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
 		// const { state } = store.getState();
-		return class myConnect extends Component {
+		return class extends Component {
 			constructor(props) {
 				super(props);
-				this.state = {
-					...mapStateToProps(store.getState()),
-					...mapDispatchToProps(store.dispatch),
-				};
 			}
 			componentDidMount() {
-				store.subscribe(() => {
-					this.setState(mapStateToProps(store.getState()));
+				this.unsubscribe = store.subscribe(() => {
+					this.forceUpdate();
 				});
 			}
 
+			componentWillUnmount() {
+				this.unsubscribe();
+			}
+
 			render() {
-				return <WrappedComponent {...this.props} {...this.state} />;
+				return (
+					<WrappedComponent
+						{...this.props}
+						{...mapStateToProps(store.getState())}
+						{...mapDispatchToProps(store.dispatch)}
+					/>
+				);
 			}
 		};
 	};
